5947ecbc3363b61305121b7896adfd60b256de9c
default <bianca.bertinato@stu.ocsb.ca>
2025-10-07 23:23:14 +0000

    added math calculation

---
+++ Program 3/main.ts
@@ -0,0 +1,12 @@
+/**
+ * @author Bianca Bertinato
+ * @version 1.0.0
+ * @date 2025-10-07
+ * @fileoverview This program outputs the volume of a sphere with a radius of 4 cm.
+ */
+
+// shows calculation
+console.log("What is the volume of a sphere with a radius of 4 cm?");
+console.log("Volume = " + ((4 / 3) * Math.PI * 4 * 4 * 4) + " cm³")
+
+console.log("\nDone.");
