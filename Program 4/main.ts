1398592182cf8838f109989e326f93465337c907
default <bianca.bertinato@stu.ocsb.ca>
2025-10-07 23:39:43 +0000

    math calculation

---
+++ Program 4/main.ts
@@ -0,0 +1,12 @@
+/**
+ * @author Bianca Bertinato
+ * @version 1.0.0
+ * @date 2025-10-07
+ * @fileoverview This program calculates the gross pay for an employee, who worked 40 hours and had an hourly wage of $8.20.
+ */
+
+// shows calculation
+console.log("What is the gross pay for an employee named Fred, who worked 40 hours at an hourly wage of $8.20?");
+console.log("Gross pay = " + " $" + (40 * 8.20))
+
+console.log("\nDone.");
