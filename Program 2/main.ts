2bb8b4a21cf301d941e7aa5a2e37780960b471de
default <bianca.bertinato@stu.ocsb.ca>
2025-10-07 23:14:43 +0000

    added mathematical calculation

---
+++ Program 2/main.ts
@@ -0,0 +1,12 @@
+/**
+ * @author Bianca Bertinato
+ * @version 1.0.0
+ * @date 2025-10-07
+ * @fileoverview This program computes and outputs the area of a circle with a radius of 14 cm.
+ */
+
+// shows calculation
+console.log("What is the area of a circle with a radius of 14 cm?");
+console.log("Area = " + (Math.PI * 14 * 14) + " cm²")
+
+console.log("\nDone.");
