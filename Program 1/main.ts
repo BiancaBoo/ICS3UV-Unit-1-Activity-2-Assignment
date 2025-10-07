811d8a6cfe8a5817e90ea62f9a42b5e089663680
default <bianca.bertinato@stu.ocsb.ca>
2025-10-07 23:02:19 +0000

    added mathematical conversion

---
+++ Program 1/main.ts
@@ -0,0 +1,12 @@
+/**
+ * @author Bianca Bertinato
+ * @version 1.0.0
+ * @date 2025-10-07
+ * @fileoverview This program converts a temperature of 34 degrees Celsius to Fahrenheit
+ */
+
+// shows conversion
+console.log("What is 34 degrees Celcius in Fahrenheit?");
+console.log("34 degrees Celcius = " + (34 * 9 / 5 + 32) + " F")
+
+console.log("\nDone.");
