5c005bab6bc06e0ffd9208a0d5e8bdef812ca16f
default <bianca.bertinato@stu.ocsb.ca>
2025-10-08 22:38:45 +0000

    added temperature conversion

---
+++ Program 1/main.go
@@ -0,0 +1,18 @@
+/*
+ * @author Bianca Bertinato
+ * @version 1.0.0
+ * @date 2025-10-08
+ * @fileoverview This program converts a temperature of 34 degrees Celsius to Fahrenheit.
+ */
+
+ package main
+
+ import "fmt"
+
+ func main() { 
+	 //shows conversion
+	 fmt.Println("What is 34 degrees Celcius in Fahrenheit?")
+	 fmt.Println("34 degrees Celcius = " + fmt.Sprint(34*9/5+32) + " F")
+
+	 fmt.Println("\nDone.")
+}
