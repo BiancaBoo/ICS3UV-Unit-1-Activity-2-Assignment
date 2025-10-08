c42161cf90a937c84a510f90ddd62fbe25dcf4cc
default <bianca.bertinato@stu.ocsb.ca>
2025-10-08 22:52:09 +0000

    added area formula

---
+++ Program 2/main.go
@@ -0,0 +1,18 @@
+/*
+ * @author Bianca Bertinato
+ * @version 1.0.0
+ * @date 2025-10-08
+ * @fileoverview This program computes and outputs the area of a circle with a radius of 14 cm.
+ */
+
+ package main
+
+ import "fmt"
+
+ func main() { 
+	 //shows conversion
+	 fmt.Println("What is the area of a circle with a radius of 14 cm?")
+	 fmt.Println("Area = " + fmt.Sprint(3.14 * 14 * 14) + " cm²")
+
+	 fmt.Println("\nDone.")
+ }
