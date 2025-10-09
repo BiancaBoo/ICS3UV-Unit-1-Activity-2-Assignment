c87a8f70d4c0507b5f4cbf0a1bc21fd26a175575
default <bianca.bertinato@stu.ocsb.ca>
2025-10-09 00:11:54 +0000

    added volume formula

---
+++ Program 3/main.go
@@ -0,0 +1,18 @@
+/*
+ * @author Bianca Bertinato
+ * @version 1.0.0
+ * @date 2025-10-08
+ * @fileoverview This program outputs the volume of a sphere with a radius of 4 cm.
+ */
+
+ package main
+
+ import "fmt"
+
+ func main() { 
+	 //shows conversion
+	 fmt.Println("What is the volume of a sphere with a radius of 4 cm?")
+	 fmt.Println("Volume = " + fmt.Sprint((4/3)* 3.14 * 4 * 4 * 4) + " cm³")
+
+	 fmt.Println("\nDone.")
+ }
